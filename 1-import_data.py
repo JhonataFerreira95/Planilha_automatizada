dfef424c11427fd4f34235a22db856a478e20e71
Bass <jhonataovirowa@gmail.com>
2024-06-09 17:52:56 -0300

    Adicoando o head, tail e value_counts

--- 1-import_data.py
+++ 1-import_data.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
+# 1-importando dados
 data = pd.read_excel("data/VendaCarros.xlsx")
-
 print(data)
+
+# 2-lista dos primeiros registros
+print(data.head(10))
+
+# 3-Lista os últimos registros
+print(data.tail())
+
+# 4-Contagem de valores por fabricante
+print(data["Fabricante"].value_counts())
