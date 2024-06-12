ea0b5b5082cb1896f782a66bf1fa76f82ab640b4
Bass <jhonataovirowa@gmail.com>
2024-06-12 15:25:04 -0300

    Criado as referências e as categorias e importado para o barchart

--- 4-add_chart.py
+++ 4-add_chart.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from openpyxl.chart import BarChart, Reference
 
 wb = load_workbook("data/pivot_table.xlsx")
 sheet = wb["Relatorio"]
@@ -7,3 +8,23 @@ min_column = wb.active.min_column
 max_column = wb.active.max_column
 min_row = wb.active.min_row
 max_row = wb.active.max_row
+
+barchart = BarChart()
+
+data = Reference(
+    sheet,
+    min_col=min_column + 1,
+    max_col=max_column,
+    min_row=min_row,
+    max_row=max_row
+)
+
+categories = Reference(
+    sheet,
+    min_col=min_column,
+    max_col=max_column,
+    min_row=min_row + 1,
+    max_row=max_row
+)
+
+barchart.add_data(data, titles_from_data=True)
