743309006d444370708ff3edf76a49b63b13704d
Bass <jhonataovirowa@gmail.com>
2024-06-12 14:47:08 -0300

    Iterado os valores por meio de loop

--- 3-Sheet_read.py
+++ 3-Sheet_read.py
@@ -1,5 +1,16 @@
 from openpyxl import load_workbook
 
 wb = load_workbook("data/pivot_table.xlsx")
-print(wb)
+sheet = wb["Relatorio"]
 
+print(sheet["A3"].value)
+print(sheet["B3"].value)
+
+for i in range(2, 6):
+    ano = sheet["A%s" %i].value
+    am = sheet["B%s" %i].value
+    bt = sheet["C%s" %i].value
+    jg = sheet["D%s" %i].value
+    mg = sheet["E%s" %i].value
+    rr = sheet["F%s" %i].value
+    print("{0} o Aston martin vendeu {1} e o Bentley vendeu {2}, jaguar vendeu {3}, MGB vendeu {4}, Rolls Royce vendeu {5}".format(ano, am, bt, jg, mg, rr))
