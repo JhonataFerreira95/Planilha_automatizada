561db16a7f37091cf9bb7c35fcaf80c6521cf8c3
Bass <jhonataovirowa@gmail.com>
2024-06-12 13:29:05 -0300

    Criado a planinlha pivô e o novo arquivo excel de relatório, dados importados como Ano, fabricante, valorvenda e sum

--- 2-pivot_table.py
+++ 2-pivot_table.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
 data = pd.read_excel("data/VendaCarros.xlsx")
-print(data)
+
+
+#DataFrame
+
+df = data[["Fabricante", "ValorVenda", "Ano"]]
+
+
+pivot_table = df.pivot_table(
+    index="Ano",
+    columns="Fabricante",
+    values="ValorVenda",
+    aggfunc="sum"
+)
+
+print(pivot_table)
+
+pivot_table.to_excel("data/pivot_table.xlsx", "Relatorio")
